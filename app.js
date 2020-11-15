9350d81c0239f0eaf11bfb791d6e900bcc645a82
TheMagicat <32884681+TheMagicat@users.noreply.github.com>
2020-11-14 17:12:25 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import $ from 'jquery';
+import requestAnimationFrame from 'raf';
+
+import audioNode from './js/audioNode.js';
+import userMedia from './js/userMedia.js';
+import graphic from './js/graphic.js';
+
+module.exports = {
+  audioNode,
+  userMedia,
+  graphic
+};
